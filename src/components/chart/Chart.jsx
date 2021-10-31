17041e5e1cf8874ac1b033c0a40720e6c33dc77b
eduardo <iam_edecarvalho@idcontractors.net>
2021-10-30 21:00:49 -0300

    Remove console.log

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -8,8 +8,7 @@ import {
      ResponsiveContainer
     } from 'recharts';
 
-export default function Chart({title, data, dataKey, grid}) {    
-  console.log(data)
+export default function Chart({title, data, dataKey, grid}) {      
     return (
     <div className="chart">
       <h3 className="chartTitle">{title}</h3>
