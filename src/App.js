86a1d72cd1c3b9050487666040ebd3512e3df195
eduardo <iam_edecarvalho@idcontractors.net>
2021-11-01 20:43:55 -0300

    add router and create userList component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/home/Home";
 import "./app.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import UserList from "./pages/userList/UserList";
 
 
 function App() {
   return (
-    <div>
+    <Router>
       <Topbar/>
       <div className="container">
         <Sidebar/>
-        <Home/>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/users">
+            <UserList />
+          </Route>
+        </Switch>
       </div>
-    </div>
+    </Router>
   );
 }
 
