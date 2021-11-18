b321b9267c46147188c189e2442638e277fe20a0
eduardo <iam_edecarvalho@idcontractors.net>
2021-11-18 19:14:55 -0300

    finishing userlist and create new user component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./app.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
+import NewUser from "./pages/newUser/NewUser";
 
 
 function App() {
@@ -17,11 +18,14 @@ function App() {
           <Route exact path="/">
             <Home />
           </Route>
-          <Route path="/users">
+          <Route exact path="/users">
             <UserList />
           </Route>
-          <Route path="/user/:userId">
+          <Route path="/users/:userId">
             <User />
+          </Route>
+          <Route path="/newUser">
+            <NewUser />
           </Route>
         </Switch>
       </div>
